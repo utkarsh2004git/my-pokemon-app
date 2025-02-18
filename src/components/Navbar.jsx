b97e26998f6026f11cba020341d4726d07cd1ceb
ukarsh2004git <utkarshshahare2004@gmail.com>
2025-02-19 01:22:21 +0530

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 import Pokeball from "../assets/Pokeball.png"
 const Navbar = () => {
     return (
-      <nav className="bg-blue-500 text-white p-4  md:px-10 flex justify-between items-center">
+      <nav className="bg-blue-500 text-white p-4 px-2  md:px-10 flex justify-between items-center">
         <h1 className="text-xl font-bold">PokéSearch</h1>
         <div>
             <p className='text-4xl hidden md:block'>Let's Catch Pokémon</p>
         </div>
+        <div className='flex items-center gap-2'>
+          <a href="https://github.com/utkarsh2004git" target='_blank'>
+          <span className='bg-gray-800 select-none cursor-pointer text-white px-2 py-1 duration-200 hover:scale-95 rounded-full'>Utkarsh</span>
+          </a>
         <img 
         src={Pokeball} 
         alt="icon" 
-        className='w-10'
+        className='w-10 cursor-pointer '
         />
+        </div>
       </nav>
     );
   };
