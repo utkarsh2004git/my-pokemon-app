f3afaeee3df1e2ed62209907d490c3368791669b
ukarsh2004git <utkarshshahare2004@gmail.com>
2025-02-19 00:46:35 +0530

    update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import Pokeball from "../assets/Pokeball.png"
 const Navbar = () => {
     return (
-      <nav className="bg-blue-500 text-white p-4 px-10 flex justify-between items-center">
+      <nav className="bg-blue-500 text-white p-4  md:px-10 flex justify-between items-center">
         <h1 className="text-xl font-bold">PokéSearch</h1>
         <div>
-            <p className='text-4xl'>Let's Catch Pokémon</p>
+            <p className='text-4xl hidden md:block'>Let's Catch Pokémon</p>
         </div>
         <img 
         src={Pokeball} 
