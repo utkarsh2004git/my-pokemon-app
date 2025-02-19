f5c51ce9c9e860c4213ac9d4eec6d6fd137c93fc
ukarsh2004git <utkarshshahare2004@gmail.com>
2025-02-19 15:18:54 +0530

    add bg-effect for each pokemon-type

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -7,6 +7,22 @@ const PokemonCard = ({ pokemon }) => {
         setIsActive((prev) => !prev);
     };
 
+    // console.log(pokemon.types?.[0].type?.name);
+    
+    const pokemonType = {
+        bug: "from-green-200 to-green-600 scale-95",
+        grass: "from-green-50 to-green-400 scale-95",
+        fire: "from-red-50 to-red-400 scale-95",
+        normal: "from-gray-50 to-gray-400 scale-95",
+        poison: "from-purple-50 to-purple-400 scale-95",
+        electric: "from-yellow-50 to-yellow-400 scale-95",
+        fairy: "from-pink-50 to-pink-400 scale-95",
+        water: "from-blue-50 to-blue-400 scale-95",
+        fighting: "from-orange-50 to-orange-400 scale-95",
+        psychic: "from-pink-50 to-purple-400 scale-95",
+        rock: "from-gray-50 to-yellow-400 scale-95",
+    };
+
     return (
         <motion.div
             initial={{ opacity: 0.9, y: 60 }}
@@ -16,8 +32,8 @@ const PokemonCard = ({ pokemon }) => {
             onClick={toggleActive}
             className={`cursor-pointer rounded-xl shadow-md shadow-blue-200 p-3 flex flex-col items-center justify-center w-56 group select-none
             bg-gradient-to-br from-blue-50 to-blue-400 transition duration-500 
-            hover:from-yellow-50 hover:to-orange-300 hover:scale-95 
-            ${isActive ? 'from-yellow-50 to-orange-300 scale-95' : ''}`}
+            group-hover:from-yellow-50 hover:to-orange-300 hover:scale-95 
+            ${isActive ?pokemonType[pokemon.types?.[0].type?.name] || "rom-gray-50 to-gray-300 scale-95" : ''}`}
         >
             <figure className="group relative">
                 <img
@@ -29,7 +45,7 @@ const PokemonCard = ({ pokemon }) => {
                     group-hover:scale-[2]  ${isActive ? 'scale-[2]' : ''}`}
                 />
                 <h1 className={`text-2xl text-center font-semibold my-2 transition-opacity duration-500 
-                group-hover:opacity-0 ${isActive ? 'opacity-0' : 'opacity-100'}`}>
+                group-hover:opacity-0  ${isActive ? 'opacity-0' : 'opacity-100'}`}>
                     {pokemon.name}
                 </h1>
             </figure>
